5d2bed71e6e043763d3f78efb5fea5585db22593
inpsrecordedbackup <inpsrecordedbackup@gmail.com>
2025-07-19 14:13:24 +0000

    removed lovable tagger

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
   server: {
     host: "::",
     port: 8080,
@@ -28,10 +27,8 @@ export default defineConfig(({ mode }) => ({
     }
   },
   plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+    react()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
